b0b968c2be4f1cfdf5896617b638c4e2a05eeae5
muzaffarvv <muzaffarxomitaliyev23@gmail.com>
2026-06-13 02:44:24 +0500

    feat: update LCM and GCD functions to support BigInteger for larger values

--- ITask3_Cs/Program.cs
+++ ITask3_Cs/Program.cs
@@ -1,12 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Numerics;
 
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/vv/muzaffarxomitaliyev23_gmail_com", (string? x, string? y) =>
 {
-    if (!long.TryParse(x, out var a) || 
-        !long.TryParse(y, out var b) ||
-        a <= 0 || b <= 0)  return "NaN";
+    if (!BigInteger.TryParse(x, out var a) || 
+        !BigInteger.TryParse(y, out var b) || 
+        a <= 0 || b <= 0)
+    {
+        return "NaN";
+    }
 
     return FindLcm(a, b).ToString();
 });
@@ -14,7 +18,7 @@ app.MapGet("/vv/muzaffarxomitaliyev23_gmail_com", (string? x, string? y) =>
 app.Run();
 return;
 
-static long FindGcd(long a, long b)
+static BigInteger FindGcd(BigInteger a, BigInteger b)
 {
     while (b != 0)
     {
@@ -22,11 +26,10 @@ static long FindGcd(long a, long b)
         a = b;
         b = temp;
     }
-
     return a;
 }
 
-static long FindLcm(long a, long b)
+static BigInteger FindLcm(BigInteger a, BigInteger b)
 {
-    return a / FindGcd(a, b) * b;
+    return (a / FindGcd(a, b)) * b;
 }
